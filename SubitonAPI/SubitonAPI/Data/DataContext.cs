f6d094c373f4f86307fdb1d31a2031926e603331
Oktawian-L <oktawianlasek@gmail.com>
2019-10-06 11:45:17 +0200

    Sqlite

---
+++ SubitonAPI/SubitonAPI/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubitonAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubitonAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        #region TableDefinitions
+        public DbSet<Values> Values { get; set; }
+        #endregion TableDefinitions
+    }
+}
