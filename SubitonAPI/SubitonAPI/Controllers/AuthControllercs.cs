117191c8841ffebb14bb9d911b7f8620fb317ff5
Oktawian-L <oktawianlasek@gmail.com>
2019-10-08 21:57:44 +0200

    Login method

--- SubitonAPI/SubitonAPI/Controllers/AuthControllercs.cs
+++ SubitonAPI/SubitonAPI/Controllers/AuthControllercs.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SubitonAPI.Data;
 using SubitonAPI.DTO;
 using SubitonAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SubitonAPI.Controllers
@@ -14,15 +18,18 @@ namespace SubitonAPI.Controllers
     public class AuthControllercs : ControllerBase
     {
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _configuration;
 
-        public AuthControllercs(IAuthRepository repository)
+        public AuthControllercs(IAuthRepository repository, IConfiguration configuration)
         {
             _repository = repository;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
         public async Task<ActionResult> Register(UserRegisterDTO userRegisterDTO)
         {
+
             userRegisterDTO.Username = userRegisterDTO.Username.ToLower();
             if (await _repository.UserExists(userRegisterDTO.Username))
             {
@@ -35,6 +42,24 @@ namespace SubitonAPI.Controllers
             };
 
             await _repository.Register(userToCreate, userRegisterDTO.Password);
+
+            return StatusCode(201);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserDTO userDTO)
+        {
+            var user = await _repository.Login(userDTO.Username.ToLower(),userDTO.Password);
+            if (user == null)
+                return Unauthorized();
+
+            //JWT
+            var jwtToken = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
             return StatusCode(201);
         }
--- SubitonAPI/SubitonAPI/DTO/UserDTO.cs
+++ SubitonAPI/SubitonAPI/DTO/UserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace SubitonAPI.DTO
 {
     public class UserDTO
     {
+        [Required(ErrorMessage ="Username is required.")]
         public string Username { get; set; }
+
+        [StringLength(12,MinimumLength =6,ErrorMessage ="Password must be between 6 and 12 signs.")]
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
     }
     public class UserRegisterDTO : UserDTO
