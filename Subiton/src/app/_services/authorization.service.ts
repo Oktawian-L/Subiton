aba1424266ea011942af21f51c05ca1df31f3e68
Oktawian-L <oktawianlasek@gmail.com>
2019-10-12 20:41:19 +0200

    rsjx added properly

--- Subiton/src/app/_services/authorization.service.ts
+++ Subiton/src/app/_services/authorization.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,12 @@ constructor(private http: HttpClient) {
 
  }
  login(model: any){
-  return this.http.post(this.baseUrl + 'login',  model);
+  return this.http.post(this.baseUrl + 'login',  model)
+    .pipe(map((response: any) => {
+      const user = response;
+      if(user){
+        localStorage.setItem('token',user.token);
+      }
+    }));
  }
 }
