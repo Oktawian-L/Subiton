aa2dec42dbcd9774bed8cd1591d74df1fe9f53c6
Oktawian-L <oktawianlasek@gmail.com>
2019-10-12 20:22:23 +0200

    Update authorization.service.ts

--- Subiton/src/app/_services/authorization.service.ts
+++ Subiton/src/app/_services/authorization.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
+  baseUrl = 'http://localhost:44363/api/auth/';
 
 constructor(private http: HttpClient) {
 
  }
-
+ login(model: any){
+  return this.http.post(this.baseUrl + 'login',  model);
+ }
 }
