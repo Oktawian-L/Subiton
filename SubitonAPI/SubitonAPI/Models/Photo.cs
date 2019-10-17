ea713e70dcc6a45af498405217b659f1421e465e
Oktawian-L <oktawianlasek@gmail.com>
2019-10-17 19:05:07 +0200

    extended photos

--- SubitonAPI/SubitonAPI/Models/Photo.cs
+++ SubitonAPI/SubitonAPI/Models/Photo.cs
@@ -10,5 +10,6 @@ namespace SubitonAPI.Models
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
+        public int set_user_nrid { get; set; }
     }
 }
