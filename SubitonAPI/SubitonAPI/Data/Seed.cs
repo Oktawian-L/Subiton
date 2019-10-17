c34ef7a79d6e1cff47a8da710717147bd0ac8fe2
Oktawian-L <oktawianlasek@gmail.com>
2019-10-17 20:08:07 +0200

    Create Seed.cs

---
+++ SubitonAPI/SubitonAPI/Data/Seed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubitonAPI.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
