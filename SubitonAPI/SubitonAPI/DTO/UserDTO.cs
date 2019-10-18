53bbdd7360c18b9113a0586ca68a1aae3555b37e
Oktawian-L <oktawianlasek@gmail.com>
2019-10-18 20:45:29 +0200

    Adding AnimalTypes

--- SubitonAPI/SubitonAPI/DTO/UserDTO.cs
+++ SubitonAPI/SubitonAPI/DTO/UserDTO.cs
@@ -23,9 +23,6 @@ namespace SubitonAPI.DTO
     public class UserForListDto: UserDTO
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
 
         // basic user info
         public string Gender { get; set; }
