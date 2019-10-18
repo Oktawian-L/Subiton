6d5dfd59ee4e788d59c8117f43ab6172f79b3e0d
Oktawian-L <oktawianlasek@gmail.com>
2019-10-18 20:22:03 +0200

    Update UsersController.cs

--- SubitonAPI/SubitonAPI/Controllers/UsersController.cs
+++ SubitonAPI/SubitonAPI/Controllers/UsersController.cs
@@ -11,7 +11,7 @@ using SubitonAPI.Models;
 
 namespace SubitonAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
