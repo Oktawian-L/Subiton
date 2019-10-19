66fa32adfccaba618c5a0acf4cfc388fa64b617b
Oktawian-L <oktawianlasek@gmail.com>
2019-10-19 11:14:13 +0200

    Calculating user age with custom Datetime method
    Calculating user age with custom Datetime method

--- SubitonAPI/SubitonAPI/Helpers/AutoMapperProfiles.cs
+++ SubitonAPI/SubitonAPI/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SubitonAPI.DTO;
+using SubitonAPI.Helpers;
 using SubitonAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -21,9 +22,18 @@ namespace SubitonAPI.Helper
                 .ForMember(dest => dest.PhotoUrl, opt =>
                 {
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
-                }
-                );
-            CreateMap<User, UserForDetailsDTO>();
+                }).ForMember(dest => dest.Age, opt => {
+                    opt.MapFrom((src, d) => src.DateOfBirth.CalculateAge());
+                });
+
+            // opening profile data
+            CreateMap<User, UserForDetailsDTO>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                }).ForMember(dest => dest.Age, opt => {
+                    opt.MapFrom((src, d) => src.DateOfBirth.CalculateAge());
+                });
             CreateMap<Photo, PhotoDTO>();
         }
     }
---
+++ SubitonAPI/SubitonAPI/Helpers/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubitonAPI.Helpers
+{
+    public static class Extensions
+    {
+        public static int CalculateAge(this DateTime datetime)
+        {
+            var age = DateTime.Today.Year - datetime.Year;
+            //if have birthdasys in current year
+            if (datetime.AddYears(age) > DateTime.Today)
+                age--;
+            return age;
+        }
+    }
+}
