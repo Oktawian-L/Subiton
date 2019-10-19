200175b19a9817ae861f8d6a4e17f14cfc34c3aa
Oktawian Lasek <oktawian.lasek@capgemini.com>
2019-10-19 13:37:21 +0200

    Init structure for wepapi tests
    Init structure for wepapi tests

---
+++ SubitonAPI/SubitonTests/UsersWebApiTest.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using SubitonAPI.Controllers;
+using SubitonAPI.Data;
+using System;
+using Xunit;
+
+namespace SubitonTests
+{
+    public class UsersWebApiTest
+    {
+        /// <summary>
+        /// The user repository
+        /// </summary>
+        private readonly IUserRepository _userRepository;
+
+        /// <summary>
+        /// The mapper
+        /// </summary>
+        private readonly IMapper _mapper;
+
+        private UsersController _usersController;
+
+        public UsersWebApiTest()
+        {
+            _service =
+            _usersController =
+        }
+
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
