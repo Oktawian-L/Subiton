f45afd5e0593f69d825aa3b7fdb5baf035d7e70c
Oktawian-L <oktawianlasek@gmail.com>
2019-10-20 17:52:48 +0200

    Update auth.guard.ts

--- Subiton/src/app/_guards/auth.guard.ts
+++ Subiton/src/app/_guards/auth.guard.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizationService } from '../_services/authorization.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthorizationService, private router: Router, private alertify: AlertifyService) {
+
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
