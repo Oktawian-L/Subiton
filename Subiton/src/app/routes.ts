4b35b8c4f3a00c14de97927a1ae8d52e09cb3a6d
Oktawian-L <oktawianlasek@gmail.com>
2019-10-20 11:08:21 +0200

    Update routes.ts

--- Subiton/src/app/routes.ts
+++ Subiton/src/app/routes.ts
@@ -1,6 +1,12 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 export const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
 ];
