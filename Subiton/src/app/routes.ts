8591e523839a903fa2d5e0c3feaf85f246cba359
Oktawian-L <oktawianlasek@gmail.com>
2019-10-20 17:58:10 +0200

    Update routes.ts

--- Subiton/src/app/routes.ts
+++ Subiton/src/app/routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { LikesComponent } from './likes/likes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'users', component: UserListComponent },
+  { path: 'users', component: UserListComponent, canActivate: [AuthGuard] },
   { path: 'likes', component: LikesComponent },
   { path: 'messages', component: MessagesComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
