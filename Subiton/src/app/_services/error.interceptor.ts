12dcfe8ea089fb36ced77213fe2e58321dafafa7
Oktawian-L <oktawianlasek@gmail.com>
2019-10-23 19:01:12 +0200

    Update error.interceptor.ts

--- Subiton/src/app/_services/error.interceptor.ts
+++ Subiton/src/app/_services/error.interceptor.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+    return next.handle(req).pipe(
+      catchError( error => {
+      if (error instanceof HttpErrorResponse) {
+        const applicationError = error.headers.get('Application-Error');
+        if (applicationError){
+          return throwError(applicationError);
+        }
+      }
+    })
+    );
   }
 
 }
