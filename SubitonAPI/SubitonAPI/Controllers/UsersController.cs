1122f659890f743ae2219528a82066bd4e82512f
Oktawian-L <oktawianlasek@gmail.com>
2019-10-27 15:01:02 +0100

    banner

--- SubitonAPI/SubitonAPI/Controllers/UsersController.cs
+++ SubitonAPI/SubitonAPI/Controllers/UsersController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SubitonAPI.Data;
 using SubitonAPI.DTO;
 using SubitonAPI.Models;
