220af081693a6a121371a58a4e4ad106dd686006
Oktawian-L <oktawianlasek@gmail.com>
2019-10-28 20:12:21 +0100

    Update user-details.resolver.ts

--- Subiton/src/app/_resolvers/user-details.resolver.ts
+++ Subiton/src/app/_resolvers/user-details.resolver.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { Resolve, Router } from '@angular/router';
 import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { RouterStateSnapshot } from '@angular/router';p
+import { Observable } from 'rxjs';
 
 @Injectable()
 
 export class UserDetailResolver implements Resolve<User> {
+
+
+  constructor(private userService: UserService,
+              private router: Router,
+              private alertify: AlertifyService ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+          : User |Observable<User> | Promise<User> {
+    throw new Error('Method not implemented.');
+  }
 
 }
 
