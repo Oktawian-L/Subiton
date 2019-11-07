14282a50090993a3cbe26025aebe1b7c9a756fbc
Oktawian-L <oktawianlasek@gmail.com>
2019-11-07 23:22:05 +0100

    Create ClaudinarySettings.cs

---
+++ SubitonAPI/SubitonAPI/Helpers/ClaudinarySettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubitonAPI.Helpers
+{
+    public class ClaudinarySettings
+    {
+        /// <summary>Gets or sets the name of the cloud.</summary>
+        /// <value>The name of the cloud.</value>
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
