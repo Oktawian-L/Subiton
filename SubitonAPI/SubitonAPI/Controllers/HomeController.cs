77ca60d1ec84d5bbe941b2f91a0ce3adfe2bf692
Oktawian-L <oktawianlasek@gmail.com>
2019-11-09 17:08:30 +0100

    HomeController

---
+++ SubitonAPI/SubitonAPI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SubitonAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+{
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public /*IActionResult*/IEnumerable<WeatherForecast> Index()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+           // return View();
+        }
+    }
+}
